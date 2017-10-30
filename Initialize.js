1b05e57583042243d21c3c78f2179cd23adfe719
avetisyanIncorporation <pargev.avetisian@gmail.com>
2017-10-30 19:02:15 +0300

    setting new heights by coordinates

--- Initialize.js
+++ Initialize.js
@@ -15,32 +15,46 @@ try{
 		});
 	viewer.dataSources.add(polygon);
 	
+	var coords=[];
+	
 	polygon.then(dataSource =>{
-			
 		var myPolygon = dataSource.entities.getById('myPolygonExample');
 		//var myPolygon = dataSource.entities.values[0];		
-		var position = myPolygon.polygon.hierarchy.getValue().positions[0];
-		//alert(Cesium.Cartographic.fromDegrees(position.x,position.y));
+		var positions = myPolygon.polygon.hierarchy.getValue().positions;
+		coords = Cesium.Ellipsoid.WGS84.cartesianArrayToCartographicArray(positions);
+		
+		alert(coords);
 	});
+	
+	setTimeout(function() {
+	
+	//var ourHeight = 100;
+	var oldHeights=[];
+	for(var i = 0; i < coords.length; i++){
+		oldHeights.push(coords[i].height);
+	}
+	
+	var promise = Cesium.sampleTerrainMostDetailed(viewer.terrainProvider, coords);
+	Cesium.when(promise, function(updatedPositions) {
+		
+		for(var i = 0; i < updatedPositions.length; i++){
+			coords[i].height += oldHeights[i];
+			//newHeight = updatedPositions[i].height + ourHeight;
+		}
+	});
+	
+	setTimeout(function() {
+		
+		alert(coords);
+		//выставить наш полигон на высоту newHeight
+		
+	},100);
+	
+	},500);
 	
 	setTimeout(function() {
 		viewer.zoomTo(polygon, new Cesium.HeadingPitchRange(0, -100, 1500)) 
 		}, 1500);
-		
-	var positions = [
-		Cesium.Cartographic.fromDegrees(45, 40)
-	];		
-	
-	var ourHeight = 10;
-	var newHeight;
-	
-	var promise = Cesium.sampleTerrainMostDetailed(viewer.terrainProvider, positions);
-	Cesium.when(promise, function(updatedPositions) {
-		newHeight = updatedPositions[0].height + ourHeight;
-		alert(newHeight);
-	});
-	
-	//выставить наш полигон на высоту newHeight
 	
 }catch(e){
 	alert(e);
